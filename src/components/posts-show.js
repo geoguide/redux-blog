9b2839babdcc2d9f1303436c5053d65fa6627631
Geoff Guidetti <geoffguidetti@gmail.com>
2018-04-23 21:27:10 -0700

    Deleting linking etc.

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
+  }
+
   componentDidMount() {
+    if(!this.props.posts) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
+  }
+
+  onDeleteClick() {
     const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    this.props.deletePost(id, () => this.props.history.push('/'));
   }
 
   render() {
+    const { post } = this.props;
+    if(!post) {
+      return <div>Loading</div>;
+    }
     return (
       <div>
-        Posts Show!
+        <Link to="/">Back to Index</Link>
+        <button
+          onClick={this.onDeleteClick}
+          className="btn btn-danger pull-xs-right">Delete Post</button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
@@ -22,4 +45,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,12 +1,16 @@
 import _ from 'lodash';
-import { FETCH_POSTS, CREATE_POST, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action = {}) {
   switch (action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
-      return { ...state, [action.payload.data.id]: action.payload.data };
+      const newState = { ...state, [action.payload.data.id]: action.payload.data };
+      return newState;
+    case DELETE_POST:
+      //return state without the object of deleted id
+      return _.omit(state, action.payload);
     default:
       return state;
 
